b1d123217094a39bbb2a1a7153c72ed3b08f5a9a
Remco Gerlich <remco@gerlich.nl>
2014-08-06 11:42:00 +0200

    Urls functions come from different module

--- lizard_progress/urls.py
+++ lizard_progress/urls.py
@@ -7,8 +7,9 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
-from django.conf.urls.defaults import include
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import include
+from django.conf.urls import patterns
+from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 

b74e91fd8c85646cbbceaf057d4012c8f08d9338
Remco Gerlich <remco@gerlich.nl>
2015-08-12 11:11:09 +0200

    WIP

--- lizard_progress/sql/publiekskaart_almere.sql
+++ lizard_progress/sql/publiekskaart_almere.sql
@@ -1,3 +1,4 @@
+DROP VIEW IF EXISTS publiekskaart_almere;
 CREATE VIEW publiekskaart_almere AS (
     SELECT
         location.location_code,
@@ -17,8 +18,8 @@ CREATE VIEW publiekskaart_almere AS (
     ON
         activity.project_id = project.id
     WHERE
-        project.organization_id = 6 -- Gemeente Almere
-        AND project.project_type_id = 31 -- Planmatig
+        project.organization_id = 3 -- Gemeente Almere is 3 on staging, 6 on production
+        AND project.project_type_id != 11 -- on Production check id == 31 (Planmatig)
         AND NOT project.is_archived
         AND (location.planned_date IS NULL OR
             location.planned_date >= current_date - interval '30 days')

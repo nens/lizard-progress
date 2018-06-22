82d12164b09220bad666346a84505afd834970fa
Dmitry Ebel <dmitry.ebel@nelen-schuurmans.nl>
2018-06-22 13:44:58 +0200

    fixed wrong operator in test

--- lizard_progress/util/tests/test_autoreviewer.py
+++ lizard_progress/util/tests/test_autoreviewer.py
@@ -92,7 +92,7 @@ class TestAutoReviewerObservation(FixturesTestCase):
                       Observation([Field('A', 'BBB'), Field('D', '4'), Field('F', '6')]): 'WARN',
                       Observation([Field('A', 'BBB'), Field('D', '13'), Field('H', '6')]): 'INTERVENE',
                       Observation([Field('A', 'BAF'), Field('B', 'Z'), Field('C', 'Z')]): 'WARN',
-                      Observation([Field('A', 'BZF'), Field('B', 'Z'), Field('C', 'Z')]): 'NORULE',
+                      Observation([Field('A', 'BZF'), Field('B', 'Z'), Field('C', 'Z')]): 'NOACTION',
                       Observation([Field('A', 'BAO'), Field('R', '0'), Field('G', '1')]): 'NOACTION',
                       Observation([Field('A', 'BAO'), Field('R', '0'), Field('G', '0')]): 'INTERVENE'}
 
@@ -141,7 +141,7 @@ class TestAutoReviewerRIBX(FixturesTestCase):
             for zc in pipe['ZC']:
 
                 if zc['A'] == 'BAA':
-                    if int(zc['D']) > 10:
+                    if int(zc['D']) >= 10:
                         self.assertEquals(zc['Herstelmaatregel'], 'Ingrijp')
                     elif int(zc['D']) > 5:
                         self.assertEquals(zc['Herstelmaatregel'], 'Waarschuwing')
